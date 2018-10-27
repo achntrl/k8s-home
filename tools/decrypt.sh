51b2baa52af1f2e507c5612b8d90df0b03374b03
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2018-10-27 16:38:04 +0200

    Use AES256

--- tools/decrypt.sh
+++ tools/decrypt.sh
@@ -4,4 +4,4 @@ set -e
 
 encrypted_file=${1}
 decrypted_file=${encrypted_file%.enc}
-openssl des -d -in ${encrypted_file} -out ${decrypted_file}
+openssl aes256 -d -in ${encrypted_file} -out ${decrypted_file}
--- tools/encrypt.sh
+++ tools/encrypt.sh
@@ -3,5 +3,4 @@
 set -e
 
 source_file=${1}
-openssl des -in ${source_file} -out ${source_file}.enc
-rm ${source_file}
+openssl aes256 -in ${source_file} -out ${source_file}.enc
